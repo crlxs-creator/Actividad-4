366be506538f06bebed51b8f10fafdc28061d764
cxrls <carlosmendoza.2803@gmail.com>
2026-02-16 09:49:00 -0600

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,15 @@ const authRoutes = require('./routes/authRoutes');
 app.use('/api/albums', albumRoutes);
 app.use('/api/auth', authRoutes);
 
-// Servir frontend
 app.use(express.static(path.join(__dirname, '../../frontend')));
+
+
+app.get('*', (req, res) => {
+  if (!req.originalUrl.startsWith('/api')) {
+    res.sendFile(path.join(__dirname, '../../frontend/index.html'));
+  } else {
+    res.status(404).json({ message: 'API route not found' });
+  }
+});
 
 module.exports = app;
