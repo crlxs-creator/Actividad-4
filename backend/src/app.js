31cc99959201a3a0703420a85bf879e4b40db5a9
cxrls <carlosmendoza.2803@gmail.com>
2026-02-16 09:54:44 -0600

    Fix 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,13 +7,15 @@ app.use(express.json());
 const albumRoutes = require('./routes/albumRoutes');
 const authRoutes = require('./routes/authRoutes');
 
+/* ========= APIs PRIMERO ========= */
 app.use('/api/albums', albumRoutes);
 app.use('/api/auth', authRoutes);
 
+/* ========= STATIC FRONTEND ========= */
 app.use(express.static(path.join(__dirname, '../../frontend')));
 
-
-app.get('*', (req, res) => {
+/* ========= CATCH ALL (EXPRESS 5) ========= */
+app.get('/:path(*)', (req, res) => {
   if (!req.originalUrl.startsWith('/api')) {
     res.sendFile(path.join(__dirname, '../../frontend/index.html'));
   } else {
